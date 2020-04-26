d95a3d37f2a962571d7a5d5d8316897ec9f5c538
郑玉坤 <13011039025@163.com>
2020-04-26 11:33:11 +0800

    no message

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ PDF在线预览插件（可支持分享）
 |appid|本系统的appid|string|空|Y|
 |appkey|本系统的appkey|string|空|Y|
 |fileName|例如：我已阅读xxx|string|空|N|
-|jumpHref|点击下一步跳转的地址|string|空|N|
+|jumpUrl|跳转的地址|string|空|N|
+|jumpTxt|跳转按钮文字|string|空|N|
 <br>
 
 普通PDF需传入`pdfUrl`
@@ -26,8 +27,8 @@ PDF在线预览插件（可支持分享）
 分享功能需传入以下参数
 |属性|说明|类型|默认值|是否必传|
 |:---|:---|:---|:---|:---|
-|appid|公众号的appid|string|空|N|
+|shareAppid|公众号的appid|string|空|N|
 |shareTitle|分享链接的title|string|空|N|
-|desc|分享链接的描述|string|空|N|
-|imgUrl|分享链接的小图|string|空|N|
+|shareDesc|分享链接的描述|string|空|N|
+|shareImgUrl|分享链接的小图|string|空|N|
 <br>
--- src/view/pdfPre.vue
+++ src/view/pdfPre.vue
@@ -12,13 +12,13 @@
       </div>
       <div slot="bottom" class="mint-loadmore-bottom bottomTxt">{{showAllImg?'已加载所有图片':'加载中，请您稍候'}}</div>
     </mt-loadmore>
-    <div class="floatBtm" v-if="showCheckBOx">
+    <div class="floatBtm" v-if="showCheckBOx&&fileName">
       <div class="checkBox">
         <i :class="checkState?'checkIcon':'checkIcon1'" @click="changeState()"></i>
         <span class="checkText">{{fileName}}</span>
       </div>
       <div class="btnBox">
-        <div class="nextBtn" @click="nextBtn">下一步</div>
+        <div class="nextBtn" @click="nextBtn">{{jumpTxt}}</div>
       </div>
     </div>
   </div>
@@ -40,7 +40,6 @@ export default {
   data() {
     //vue 数据
     return {
-      name: "", //勾选框后内容
       checkState: false, //勾选框状态
       startNum: 0,
       endNum: 2,
@@ -48,7 +47,19 @@ export default {
       allLoaded: false, //是否全部加载完
       pdfImgList: [], //图片数组
       showCheckBOx: false, //展示勾选框
-      showAllImg: false //是否加载完所有
+      showAllImg: false, //是否加载完所有
+      title: "", //当前页面的名字
+      appid: "", //调用预览组件项目的appid
+      appkey: "", //调用预览组件项目的appkey
+      fileName: "", //例如：我已阅读xxx
+      jumpUrl: "", //点击下一步跳转的地址
+      jumpTxt: "", //跳转按钮文字
+      pdfUrl: "", //普通PDF的链接地址
+      businessNo: "", //业务号
+      shareAppid: "", //公众号的appid
+      shareTitle: "", //分享链接的title
+      shareDesc: "", //分享链接的描述
+      shareImgUrl: "" //分享链接的小图
     };
   },
   components: {},
@@ -73,21 +84,19 @@ export default {
     },
     //获取PDF图片
     getPdfImgList: function(num) {
-      const { headInfo } = this.$route.query;
-      let pdfPreInfo = JSON.parse(localStorage.getItem("pdfPreInfo"));
       let info = {
         head: {
           transCode: "PT600666",
           transDate: getDate(),
           transTime: getHms(),
           transId: new Date().getTime(),
-          appid: headInfo ? headInfo.appid : "WECHAT-APP-SERVER",
-          appkey: headInfo ? headInfo.appkey : "DwRaJi6hMN",
+          appid: this.appid,
+          appkey: this.appkey,
           sign: "E8TRB2A0ZZ7LQ5QT503J3FIPR4PDVZ"
         },
         supplierId: "100001", //供应商ID
-        businessNo: pdfPreInfo ? pdfPreInfo.businessNo : "8088577873102568", //业务号
-        pdfUrl: pdfPreInfo ? pdfPreInfo.pdfUrl : "",//pdf链接
+        businessNo: this.businessNo, //业务号
+        pdfUrl: this.pdfUrl, //pdf链接
         scaling: "2.5", //缩放比例
         reqType: "2", //转换类型
         imageType: "jpg", //图片类型
@@ -117,67 +126,89 @@ export default {
     },
     //下一步
     nextBtn: function() {
-      let pdfPreInfo = JSON.parse(localStorage.getItem("pdfPreInfo"));
-      const { title, headInfo, pdfInfo, policyInfo } = this.$route.query;
-      this.name = policyInfo
-        ? pdfPreInfo.fileName
-        : "我已查阅电子保险合同并阅读其内容";
-      if (this.checkState && pdfPreInfo.jumpHref) {
+      if (this.checkState) {
         let info = {
           head: {
             transCode: "MSL6068",
             transDate: getDate(),
             transTime: getHms(),
             transId: new Date().getTime(),
-            appid: headInfo ? headInfo.appid : "WECHAT-APP-SERVER",
-            appkey: headInfo ? headInfo.appkey : "DwRaJi6hMN",
+            appid: this.appid ,
+            appkey: this.appkey ,
             sign: "E8TRB2A0ZZ7LQ5QT503J3FIPR4PDVZ"
           },
           supplierId: "100001", //供应商ID
-          tenantId: pdfPreInfo ? pdfPreInfo.tenantId : "2017032717500543538",
-          businessNo: pdfPreInfo ? pdfPreInfo.businessNo : "8088577873102568", //业务号
+          tenantId: "2017032717500543538",
+          businessNo: this.businessNo, //业务号
           whetherCheck: "Y",
           checkTime: moment(new Date()).format("YYYY-MM-DD hh:mm:ss")
         };
         postJsonML("order/receiptCheckRead", info, data => {
           if (data.resultCode == "10") {
-            window.location.href = pdfPreInfo.jumpHref;
+            window.location.href = pdfPreInfo.jumpUrl;
           } else {
             alert(data.resultMsg);
           }
         });
       } else {
-        alert("请勾选'" + this.name + "'");
+        alert(
+          "请勾选'" + this.fileName
+            ? this.fileName
+            : "我已查阅电子保险合同并阅读其内容" + "'"
+        );
       }
     }
   },
   mounted: function() {
-    /* 
-    title:页面名字
-    headInfo:请求头信息
-    pdfInfo:PDF信息
-    policyInfo:保单信息
-    */
     const {
       title,
-      headInfo,
-      pdfInfo,
-      policyInfo,
-      shareInfo
+      appid,
+      appkey,
+      fileName,
+      jumpUrl,
+      jumpTxt,
+      pdfUrl,
+      businessNo,
+      shareAppid,
+      shareTitle,
+      shareDesc,
+      shareImgUrl
     } = this.$route.query;
-    localStorage.setItem("pdfPreInfo", pdfInfo ? pdfInfo : policyInfo);
-    if(title){
-      localStorage.setItem("pdfPreInfo", pdfInfo ? pdfInfo : policyInfo);
+    this.title = decodeURIComponent(title);
+    this.appid = appid?decodeURIComponent(appid):"WECHAT-APP-SERVER";
+    this.appkey = appkey?decodeURIComponent(appkey):"DwRaJi6hMN";
+    this.fileName = fileName?decodeURIComponent(fileName):"";
+    this.jumpUrl = jumpUrl?decodeURIComponent(jumpUrl):"";
+    this.jumpTxt = jumpTxt ? decodeURIComponent(jumpTxt) : "";
+    this.pdfUrl = pdfUrl?decodeURIComponent(pdfUrl):"";
+    this.businessNo = businessNo?businessNo:"";
+    this.shareAppid = shareAppid?decodeURIComponent(shareAppid):"";
+    this.shareTitle = shareTitle?decodeURIComponent(shareTitle):"";
+    this.shareImgUrl = shareImgUrl?decodeURIComponent(shareImgUrl):"";
+    this.shareDesc = shareDesc?decodeURIComponent(shareDesc):"";
+    if (pdfUrl) {
+      localStorage.removeItem("pdfPreNextUrl");
     }
-    document.title = title ? title : "查看电子保单";
+    if (businessNo && jumpUrl) {
+      localStorage.setItem("pdfPreNextUrl", decodeURIComponent(jumpUrl));
+    }
+    let pdfPreNextUrl = localStorage.getItem("pdfPreNextUrl");
+    if (businessNo && jumpTxt) {
+      this.jumpUrl = pdfPreNextUrl;
+    }
+    document.title = title ? this.title : "查看电子保单";
     this.getPdfImgList("1");
-    wxConfig({
-      appId: shareInfo ? shareInfo.appid : "wxfee39ae209983e48",
-      title: shareInfo ? shareInfo.shareTitle:"电子保单查看", // 分享标题
-      desc:shareInfo ? shareInfo.desc:"尊敬的先生/女士,请您查阅电子保单。", // 分享描述
-      imgUrl: shareInfo ? shareInfo.imgUrl : "",
-      shareUrl: window.location.href
-    });
+    if (shareAppid && shareTitle && shareTitle && shareImgUrl && shareDesc) {
+      wxConfig({
+        appId: this.shareAppid ? this.shareAppid : "wxfee39ae209983e48",
+        title: this.shareTitle ? shareInfo.shareTitle : "电子保单查看", // 分享标题
+        desc: this.shareDesc
+          ? this.shareDesc
+          : "尊敬的先生/女士,请您查阅电子保单。", // 分享描述
+        imgUrl: this.shareImgUrl ? this.shareImgUrl : "",
+        shareUrl: window.location.href
+      });
+    }
   }
 };
 </script>
